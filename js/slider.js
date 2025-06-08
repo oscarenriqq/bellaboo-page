9fe79c1fc27081b511cb4985ee9a7b7634cfa9a4
Oscar Molinar <it1@combilift.es>
2025-06-08 10:15:04 -0500

    Ajustes varios. Mobile slider, layout galería, img footer a webp

--- js/slider.js
+++ js/slider.js
@@ -1,9 +1,17 @@
 const swiper = new Swiper('.swiper', {
     loop: true,
+    slidesPerView: 1,
+    spaceBetween: 10,
     navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
     },
+    breakpoints: {
+        768: {
+          slidesPerView: 3,
+          spaceBetween: 20,
+        }
+    }
 });
 
 // Modal gallery logic
