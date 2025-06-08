76d56ff5bcbf232f00e75e37f9d82dfb787cbf77
Oscar Molinar <it1@combilift.es>
2025-06-08 10:51:05 -0500

    Agregando alertas de success y error al formulario

--- js/form.js
+++ js/form.js
@@ -1,22 +1,33 @@
-document.getElementById("contact-form").addEventListener("submit", function(event) {
+document.getElementById("contact-form").addEventListener("submit", async function(event) {
     event.preventDefault();
+
     const formData = new FormData(event.target);
-    
     const data = Object.fromEntries(formData.entries());
-    console.log("Form submitted:", data);
-    
-    
-    event.target.reset();
 
-    const URL = "https://hook.us1.make.com/onjwm5ga309aerejgd0d6tqgn3iq3i1w"
+    const URL = "https://hook.us1.make.com/onjwm5ga309aerejgd0d6tqgn3iq3i1w";
 
-    fetch(URL, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(data)
-    })
+    try {
+        const response = await fetch(URL, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(data)
+        });
 
-    alert("Form enviado")
+        if (!response.ok) {
+            throw new Error("Network response was not ok");
+        }
+
+        event.target.reset();
+        document.getElementById("alert-form-success").classList.remove("hidden");
+        setTimeout(() => {
+            document.getElementById("alert-form-success").classList.add("hidden");
+        }, 5000);
+    } catch (error) {
+        document.getElementById("alert-form-error").classList.remove("hidden");
+        setTimeout(() => {
+            document.getElementById("alert-form-error").classList.add("hidden");
+        }, 5000);
+    }
 });
